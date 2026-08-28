06e900caee281ea498ff3c33c63bec3cd08e0748
jorge andres <146102078+jorgefernandez667@users.noreply.github.com>
2026-08-28 15:48:27 -0600

    creando la arquitectura

---
+++ SistemaTurnos/src/main/java/cr/ac/una/sistematurnos/SistemaTurnos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.una.sistematurnos;
+
+/**
+ *
+ * @author ferna
+ */
+public class SistemaTurnos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
